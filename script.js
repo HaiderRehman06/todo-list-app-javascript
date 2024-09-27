99ee3cae8fc5d7b56f8b7d351b7d401bdc6ba44b
Haider Rehman <haiderrehman500@gmail.com>
2024-09-27 20:12:44 +0500

    adding javascript in js file

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+function addTask() {
+    var taskInput = document.getElementById('taskInput');
+    var taskList = document.getElementById('taskList');
+
+    if (!taskInput.value.trim()) {
+        return alert("Input can't be empty!")
+    }
+
+    var li = document.createElement('li');
+    li.textContent = taskInput.value;
+
+    var removeButton = document.createElement('button');
+    removeButton.textContent = 'Remove';
+    removeButton.className = 'remove-btn';
+    removeButton.onclick = function () {
+        taskList.removeChild(li);
+    };
+
+    li.onclick = function () {
+        li.classList.toggle('completed');
+    };
+
+    li.appendChild(removeButton);
+
+    taskList.appendChild(li);
+
+    taskInput.value = '';
+}
 
